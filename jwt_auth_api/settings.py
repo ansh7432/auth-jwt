7734ed9a6f6af88ea1f59d0629ef28f638c20d5d
Ishaan Soni <ishaan743@Ishaans-Mac-Mini.local>
2025-07-09 06:59:54 +0530

    fixed cors origin deployment

--- jwt_auth_api/settings.py
+++ jwt_auth_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-egx66x%2!x4vp9q9h_dh*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0,*.run.app').split(',')
 
 
 # Application definition
